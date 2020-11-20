eb45cc04841d80cb925de94040a6a66a4dc8ff65
smirko-dev <smirko@arcor.de>
2020-11-20 15:49:09 +0100

    Added language support for settings

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,16 +1,25 @@
 
 function settingsFunc(props){
+import { gettext } from "i18n";
+
+function settingsFunc(props) {
+  let title = gettext("title");
+  let steps = gettext("steps");
+  let dist = gettext("dist");
+  let floors = gettext("floors");
+  let cal = gettext("cal");
   return (
     <Page>
         <Section>
             <Select 
                 label={`User activity`}
+                label={`${title}`}
                 settingsKey="activity"
                 options={[
-                    { name: "Steps", value: "steps" },
-                    { name: "Distance", value: "distance" },
-                    { name: "Stairs", value: "stairs" },
-                    { name: "Calories", value: "calories" }
+                    { name: `${steps}`, value: "steps" },
+                    { name: `${dist}`, value: "distance" },
+                    { name: `${floors}`, value: "floors" },
+                    { name: `${cal}`, value: "calories" }
                 ]}
             />
         </Section>
