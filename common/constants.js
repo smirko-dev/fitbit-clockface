3f4c3a8f306463af9ea1246c395c857250526996
smirko-dev <smirko@arcor.de>
2021-01-09 07:16:46 +0100

    Added color settings

--- common/constants.js
+++ common/constants.js
@@ -1,3 +1,5 @@
 export const dataType = "cbor";
 export const dataFile = "appointments.cbor";
+export const settingsType = "cbor";
+export const settingsFile = "settings.cbor";
 export const millisecondsPerMinute = 1000 * 60;
--- settings/index.jsx
+++ settings/index.jsx
@@ -14,11 +14,25 @@ function settingsFunc(props) {
                 label={`${title}`}
                 settingsKey="activity"
                 options={[
-                    { name: `${steps}`, value: "steps" },
-                    { name: `${dist}`, value: "distance" },
-                    { name: `${floors}`, value: "floors" },
-                    { name: `${cal}`, value: "calories" }
+                    { name: `${steps}`, value: 'steps' },
+                    { name: `${dist}`, value: 'distance' },
+                    { name: `${floors}`, value: 'floors' },
+                    { name: `${cal}`, value: 'calories' }
                 ]}
+                onSelection={(value) => console.log(value)}
+            />
+            <ColorSelect
+                label="Color"
+                settingsKey="color"
+                colors={[
+                    {color: '#2490DD'},
+                    {color: '#8f32a8'},
+                    {color: '#f5882a'},
+                    {color: '#2ab0ae'},
+                    {color: '#ed1abc'},
+                    {color: 'silver'}
+                ]}
+                onSelection={(color) => console.log(color)}
             />
         </Section>
     </Page>
