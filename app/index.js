ffcf49535726a7312ccbf6f841442af54697615b
smirko-dev <64351017+smirko-dev@users.noreply.github.com>
2021-11-11 14:01:42 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -159,6 +159,9 @@ function updateActivity() {
   else if (settings.activity === 'calories') {
     activityLabel.text = today.adjusted.calories;
   }
+  else if (settings.activity === 'azm') {
+    infoLabel.text = today.adjusted.activeZoneMinutes.total || 0;
+  }
   else {
     activityLabel.text = today.adjusted.steps;
   }
